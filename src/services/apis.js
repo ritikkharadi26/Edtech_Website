d4cb124d6386448996570ebd073318f1f5074b5b
ritikkharadi <ritikkharadi597@gmail.com>
2024-07-14 09:00:48 +0530

    Base URL updated

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
-BASE_URL = "http://localhost:4000/api/v1";
 
 
+const BASE_URL = "http://localhost:4000/api/v1";
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
