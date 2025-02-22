01f57689edd4fbc161aaae4ce3e8476dc9b043b6
ritikkharadi <ritikkharadi597@gmail.com>
2025-02-22 23:33:59 +0530

    added baseurl

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,5 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-
-const BASE_URL = "http://localhost:4000/api/v1" ||"https://edu-plus-full-stack-project-using-mern-rrdt.vercel.app/";
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
